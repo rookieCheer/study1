fbc2599b26b836844d1dba1985453dd434cd083e
lrl <ruili.li@xinhuajindian.com>
2018-02-05 15:31:47 +0800

    update

--- huoqAdmin/admin/com/huoq/admin/product/bean/InvestorsBean.java
+++ huoqAdmin/admin/com/huoq/admin/product/bean/InvestorsBean.java
@@ -43,47 +43,44 @@ public class InvestorsBean {
     public PageUtil<Investors> findInvertorses(PageUtil pageUtil, String productTitle, String status, String name, String insertTime) throws Exception {
         List<Object> list = new ArrayList<Object>();
         StringBuffer buffer = new StringBuffer();
-        buffer.append(" SELECT * FROM ( ");
-        buffer.append(" SELECT u.username,us.real_name,u.regist_channel,MIN( t.pay_time),i.pay_time, p.title,p.lcqx tzts,p.lcqx , ");
-        buffer.append(" i.finish_time syts,i.investor_status,i.copies,i.in_money*0.01,i.expect_earnings/100,i.coupon*0.01,c.note,  ");
-        buffer.append(" i.coupon_shouyi/100,i.hongbao,c.type,i.annual_earnings,(i.expect_earnings/100+i.coupon_shouyi/100), ");
-        buffer.append(" i.pay_time zfsj,i.start_time,i.clear_time,i.finish_time  ");
-        buffer.append(" FROM  investors i  ");
+        buffer.append(" SELECT u.username,us.real_name,u.regist_channel,MIN(t.pay_time), p.title,p.lcqx,  ");
+        buffer.append(" i.investor_status,i.copies,i.in_money,i.expect_earnings,i.coupon, ");
+        buffer.append(" c.type,c.note,i.coupon_shouyi,i.hongbao,i.annual_earnings,  ");
+        buffer.append(" i.pay_time,i.start_time,i.clear_time,i.finish_time ");
+        buffer.append(" FROM investors i ");
         buffer.append(" LEFT JOIN product p ON i.product_id=p.id ");
         buffer.append(" LEFT JOIN users_info us  ON i.users_id=us.users_id  ");
         buffer.append(" LEFT JOIN users u  ON i.users_id=u.id  ");
-        buffer.append(" LEFT JOIN  coupon c ON p.id=c.product_id  ");
+        buffer.append(" LEFT JOIN coupon c ON c.users_id=i.users_id  ");
         buffer.append(" LEFT JOIN (SELECT v.pay_time ,v.users_id FROM investors v GROUP BY v.users_id ORDER BY v.pay_time ASC) t  ");
         buffer.append(" ON t.users_id = i.users_id  ");
-        buffer.append(" GROUP BY i.users_id,i.pay_time  ORDER BY i.pay_time DESC  ");
-        buffer.append(" )a WHERE 1=1 ");
+        buffer.append(" GROUP BY i.users_id,i.pay_time  HAVING 1=1 ");
         if (!"all".equals(status)) {
-            buffer.append(" AND a.investor_status = ?");
+            buffer.append(" AND i.investor_status = ?");
             list.add(status);
         } else {
-            buffer.append(" AND a.investor_status != 5 ");
+            buffer.append(" AND i.investor_status != 5 ");
 
         }
         if (!QwyUtil.isNullAndEmpty(name)) {
-            buffer.append(" AND a.username = ? ");
-
+            buffer.append(" AND u.username = ? ");
             list.add(DESEncrypt.jiaMiUsername(name));
         }
         // 充值时间
         if (!QwyUtil.isNullAndEmpty(insertTime)) {
             String[] time = QwyUtil.splitTime(insertTime);
             if (time.length > 1) {
-                buffer.append(" AND a.pay_time >= ? ");
+                buffer.append(" AND i.pay_time >= ? ");
 
                 list.add(QwyUtil.fmMMddyyyy.parse(time[0] + " 00:00:00"));
-                buffer.append(" AND a.pay_time <= ? ");
+                buffer.append(" AND i.pay_time <= ? ");
 
                 list.add(QwyUtil.fmMMddyyyyHHmmss.parse(time[1] + " 23:59:59"));
             } else {
-                buffer.append(" AND a.pay_time >= ? ");
+                buffer.append(" AND i.pay_time >= ? ");
 
                 list.add(QwyUtil.fmMMddyyyyHHmmss.parse(time[0] + " 00:00:00"));
-                buffer.append(" AND a.pay_time <= ? ");
+                buffer.append(" AND i.pay_time <= ? ");
 
                 list.add(QwyUtil.fmMMddyyyyHHmmss.parse(time[0] + " 23:59:59"));
             }
@@ -91,8 +88,7 @@ public class InvestorsBean {
         if (!QwyUtil.isNullAndEmpty(productTitle)) {
             buffer.append(" AND p.title like '%" + productTitle + "%' ");
         }
-
-
+        buffer.append(" ORDER BY i.pay_time DESC  ");
         StringBuffer bufferCount = new StringBuffer();
         bufferCount.append(" SELECT COUNT(*)  ");
         bufferCount.append(" FROM (");
@@ -106,29 +102,34 @@ public class InvestorsBean {
 
     }
 
+    /**
+     * 转换
+     * @param list
+     * @return
+     * @throws ParseException
+     */
     private List<Investors> toInvestors(List<Object[]> list) throws ParseException {
         List<Investors> investorsList = new ArrayList<Investors>();
         if (!QwyUtil.isNullAndEmpty(list)) {
             for (Object[] object : list) {
                 Investors investors = new Investors();
                 if (!QwyUtil.isNullAndEmpty(object)) {
-                    investors.setUsername(DESEncrypt.jieMiUsername(object[0] + ""));
-                    investors.setRealname(object[1] + "");
-                    investors.setRegistChannel(object[2] + "");
-                    if (!QwyUtil.isNullAndEmpty(object[3]) && !QwyUtil.isNullAndEmpty(object[3])) {
-                        if (object[3].toString().equals(object[4].toString())) {
+                    investors.setUsername(!QwyUtil.isNullAndEmpty(object[0]) ? DESEncrypt.jieMiUsername(object[0] + ""):"");
+                    investors.setRealname(!QwyUtil.isNullAndEmpty(object[1]) ? object[1] + "" :"");
+                    investors.setRegistChannel(!QwyUtil.isNullAndEmpty(object[2]) ? object[2] + "" :"");
+                    if (!QwyUtil.isNullAndEmpty(object[3]) && !QwyUtil.isNullAndEmpty(object[16])) {
+                        if (object[3].toString().equals(object[16].toString())) {
                             investors.setIsFirstInvt("是");
                         } else {
                             investors.setIsFirstInvt("否");
                         }
                     }
-
-                    investors.setTitle(object[5] + "");
-                    investors.setTits(!QwyUtil.isNullAndEmpty(object[6]) ? Integer.valueOf(object[6] + "") : 0);
-                    investors.setLcqx(!QwyUtil.isNullAndEmpty(object[7]) ? Integer.valueOf(object[7] + "") : 0);
-                    if (!QwyUtil.isNullAndEmpty(object[8])) {
+                    investors.setTitle(!QwyUtil.isNullAndEmpty(object[4]) ? object[4] + "" :"");
+                    investors.setTits(!QwyUtil.isNullAndEmpty(object[5]) ? Integer.valueOf(object[5] + "") : 0);
+                    investors.setLcqx(!QwyUtil.isNullAndEmpty(object[5]) ? Integer.valueOf(object[5] + "") : 0);
+                    if (!QwyUtil.isNullAndEmpty(object[19])) {
                         Date date = new Date();
-                        String time = object[8] + "";
+                        String time = object[19] + "";
                         SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
                         Date parse = sd.parse(time);
                         long day = 0;
@@ -140,40 +141,42 @@ public class InvestorsBean {
                             investors.setTzqx(day);
                         }
                     }
-                    if (!QwyUtil.isNullAndEmpty(object[9])) {
-                        investors.setInvestorStatus(object[9] + "");
+                    if (!QwyUtil.isNullAndEmpty(object[6])) {
+                        investors.setInvestorStatus(object[6] + "");
                     }
-                    investors.setCopies(!QwyUtil.isNullAndEmpty(object[10]) ? Long.valueOf(object[10] + "") : 0);
-                    investors.setInMoney(!QwyUtil.isNullAndEmpty(object[11]) ? Double.valueOf(object[11] + "") : 0.0);
-                    investors.setExpectEarnings(!QwyUtil.isNullAndEmpty(object[12]) ? Double.valueOf(object[12] + "") : 0.0);
-                    investors.setCoupon(!QwyUtil.isNullAndEmpty(object[13]) ? Double.valueOf(object[13] + "") : 0.0);
-                    if (!QwyUtil.isNullAndEmpty(object[13])) {
-                        Double coupon = Double.valueOf(object[13] + "");
+                    investors.setCopies(!QwyUtil.isNullAndEmpty(object[7]) ? Long.valueOf(object[7] + "") : 0);
+                    investors.setInMoney(!QwyUtil.isNullAndEmpty(object[8]) ? Double.valueOf(object[8] + "")*0.01 : 0.0);
+                    investors.setExpectEarnings(!QwyUtil.isNullAndEmpty(object[9]) ? Double.valueOf(object[9] + "")*0.01 : 0.00);
+                    investors.setCoupon(!QwyUtil.isNullAndEmpty(object[10]) ? Double.valueOf(object[10] + "")*0.01 : 0.0);
+                    if (!QwyUtil.isNullAndEmpty(object[10])) {
+                        Double coupon = Double.valueOf(object[10] + "")*0.01;
                         if (coupon > 0) {
-                            if ("1".equals(object[17]) || "0".equals(object[17])) {
-                                investors.setInvestSource(!QwyUtil.isNullAndEmpty(object[14]) ? object[14].toString() : null);
+                            if ("1".equals(object[11]) || "0".equals(object[11])) {
+                                investors.setInvestSource(!QwyUtil.isNullAndEmpty(object[12]) ? object[12].toString() : null);
                             }
                         }
                     }
-                    if (!QwyUtil.isNullAndEmpty(object[16])) {
-                        if (!QwyUtil.isNullAndEmpty(object[13])) {
-                            Double hongbao = Double.valueOf(object[13] + "");
+                    if (!QwyUtil.isNullAndEmpty(object[14])) {
+                            Double hongbao = Double.valueOf(object[14] + "");
                             if (hongbao > 0) {
-                                if ("3".equals(object[17])) {
-                                    investors.setRedPackageSource(!QwyUtil.isNullAndEmpty(object[14]) ? object[14].toString() : null);
+                                if ("3".equals(object[11])) {
+                                    investors.setRedPackageSource(!QwyUtil.isNullAndEmpty(object[12]) ? object[12].toString() : null);
                                 }
                             }
                         }
+                    investors.setCouponShouyi(!QwyUtil.isNullAndEmpty(object[13]) ? Double.valueOf(object[13] + "")*0.01 : 0.0);
+                    investors.setHongbao(!QwyUtil.isNullAndEmpty(object[14]) ? Double.valueOf(object[14] + "") : 0.0);
+                    investors.setAnnualEarnings(!QwyUtil.isNullAndEmpty(object[15]) ? Double.valueOf(object[15] + "") : 0.0);
+                    if(!QwyUtil.isNullAndEmpty(object[9]) && !QwyUtil.isNullAndEmpty(object[13]) ){
+                        Double expectEarnings= Double.valueOf(object[9]+"")*0.01;
+                        Double couponShouyi= Double.valueOf(object[13]+"")*0.01;
+                        Double sum=expectEarnings+couponShouyi;
+                        investors.setFinalEarnings(sum);
                     }
-
-                    investors.setCouponShouyi(!QwyUtil.isNullAndEmpty(object[15]) ? Double.valueOf(object[15] + "") : 0.0);
-                    investors.setHongbao(!QwyUtil.isNullAndEmpty(object[16]) ? Double.valueOf(object[16] + "") : 0.0);
-                    investors.setAnnualEarnings(!QwyUtil.isNullAndEmpty(object[18]) ? Double.valueOf(object[18] + "") : 0.0);
-                    investors.setFinalEarnings(!QwyUtil.isNullAndEmpty(object[19]) ? Double.valueOf(object[19] + "") : 0.0);
-                    investors.setPayTime(!QwyUtil.isNullAndEmpty(object[20]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[20].toString()) : null);
-                    investors.setStartTime(!QwyUtil.isNullAndEmpty(object[21]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[21].toString()) : null);
-                    investors.setClearTime(!QwyUtil.isNullAndEmpty(object[22]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[22].toString()) : null);
-                    investors.setFinishTime(!QwyUtil.isNullAndEmpty(object[23]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[23].toString()) : null);
+                    investors.setPayTime(!QwyUtil.isNullAndEmpty(object[16]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[16].toString()) : null);
+                    investors.setStartTime(!QwyUtil.isNullAndEmpty(object[17]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[17].toString()) : null);
+                    investors.setClearTime(!QwyUtil.isNullAndEmpty(object[18]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[18].toString()) : null);
+                    investors.setFinishTime(!QwyUtil.isNullAndEmpty(object[19]) ? QwyUtil.fmyyyyMMddHHmmss.parse(object[19].toString()) : null);
                     investorsList.add(investors);
                 }
             }
